a7b2b874d3707af68b319ebddd6b18f8d0ff4b52
Zachary Nathan Goode <zachary@the-goodes.com>
2020-06-21 20:51:28 -0700

    update

--- Source/Food-Dispenser/Food-Dispenser.ino
+++ Source/Food-Dispenser/Food-Dispenser.ino
@@ -7,10 +7,14 @@
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <SPI.h>
+#include <Servo.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <AFArray.h>
+#include <AFArrayType.h>
 #include "FS.h"
+#include "SH1106Wire.h"
+#include "SSD1306Wire.h"
+#include "OLEDDisplayUi.h"
 
 #include "HTML.h"
 
@@ -18,28 +22,92 @@
 #define HOSTNAME "Food-Dispensor"
 #define CONFIG "/conf.txt"
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
-#define INVERT_DISPLAY false
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+void handleSystemReset();
+void handleWifiReset();
+int8_t getWifiQuality();
+void writeSettings();
+void handleUpdateConfigure();
+void handleUpdateControl();
+void readSettings();
+void handleNotFound();
+void handleRoot();
+void handleConfigure();
+void handleConfigureNoPassword();
+void handleControl();
+void handleControlNoPassword();
+String reportFeedingTimes();
+void feedPosie(double cupsOfFood);
+int getTimeZone(int modes);
 
 const int WEBSERVER_PORT = 80;
 const boolean WEBSERVER_ENABLED = true;
 char* www_username = "admin";
 char* www_password = "password";
+int timeZone = -7;
 int screenMode = 0;
 boolean displayIP = false;
+
+int lengthOfFeedingArray = 0;
+AFArray<int> feedingHours;
+AFArray<int> feedingMinutes;
+AFArray<boolean> AMPM;
+AFArray<boolean> monday;
+AFArray<boolean> tuesday;
+AFArray<boolean> wednesday;
+AFArray<boolean> thursday;
+AFArray<boolean> friday;
+AFArray<boolean> saturday;
+AFArray<boolean> sunday;
+AFArray<double> cups;
 
 long previousMillis = 0;
 long previousMillisIP = 0;
 long interval = 5000;
 long IPInterval = 10000;
+long previousMillisFeedingTime = 0;
+long FeedingTimeInterval = 60000;
+long previousMillisServo = 0;
+long servoInterval = 0;
+
+boolean augerOn = false;
+
+long millisPerCup = 250; //fill this variable
 
 boolean ENABLE_OTA = true;
 String OTA_Password = "";
 
 ESP8266WebServer server(WEBSERVER_PORT);
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
+WiFiUDP ntpUDP;
+
+NTPClient timeClient(ntpUDP, "ntp1.net.berkeley.edu");
+
+// Display Settings
+const int I2C_DISPLAY_ADDRESS = 0x3c; // I2C Address of your Display (usually 0x3c or 0x3d)
+const int SDA_PIN = D2;
+const int SCL_PIN = D5;
+const boolean INVERT_DISPLAY = true; // true = pins at top | false = pins at the bottom
+//#define DISPLAY_SH1106       // Uncomment this line to use the SH1106 display -- SSD1306 is used by default and is most common
+
+// Initialize the oled display for I2C_DISPLAY_ADDRESS
+// SDA_PIN and SCL_PIN
+#if defined(DISPLAY_SH1106)
+SH1106Wire display(I2C_DISPLAY_ADDRESS, SDA_PIN, SCL_PIN);
+#else
+SSD1306Wire display(I2C_DISPLAY_ADDRESS, SDA_PIN, SCL_PIN); // this is the default
+#endif
+
+OLEDDisplayUi   ui( &display );
+
+// Set the number of Frames supported
+const int numberOfFrames = 3;
+FrameCallback frames[numberOfFrames];
+FrameCallback clockFrame[2];
+
+Servo myservo;
 
 const int externalLight = LED_BUILTIN;
 
@@ -57,16 +125,22 @@ void setup(void) {
 
   readSettings();
 
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.display();
+  //initialize display
+  display.init();
   if (INVERT_DISPLAY) {
-    display.invertDisplay(true); // connections at top of OLED display
+    display.flipScreenVertically(); // connections at top of OLED display
   }
 
-  display.clearDisplay();
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(64, 10, "Connect to WiFi Network");
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(64, 24, HOSTNAME);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(64, 38, "And log into your home");
+  display.drawString(64, 48, "WiFi network to setup");
   display.display();
-  delay(100);
-  display.clearDisplay();
 
   parseHomePage();
   parseConfigurePage();
@@ -114,6 +188,8 @@ void setup(void) {
     server.on("/forgetwifi", handleWifiReset);
     server.on("/Configure", handleConfigure);
     server.on("/updateConfig", handleUpdateConfigure);
+    server.on("/Control", handleControl);
+    server.on("/updateControl", handleUpdateControl);
     server.on("/FactoryReset", handleSystemReset);
     server.on("/WifiReset", handleWifiReset);
     server.onNotFound(handleRoot);
@@ -127,8 +203,11 @@ void setup(void) {
 }
 
 void loop() {
+  timeClient.update();
   server.handleClient();
   ArduinoOTA.handle();
+
+  unsigned long currentMillis = millis();
 
   if (digitalRead(IPAddressButton) == HIGH) { //IP Address Mode
     String tempString = "";
@@ -137,26 +216,85 @@ void loop() {
 
     displayIP = true;
 
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0, 4);
-    display.println(F("You May Connect to IP"));
-    display.setCursor(0, 13);
-    tempString = WiFi.localIP().toString();
-    display.println(tempString);
-    display.setCursor(0, 22);
-    tempString = "Port: ";
-    tempString = tempString + String(WEBSERVER_PORT);
-    display.println(tempString);
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(64, 10, "Web Interface On");
+    display.drawString(64, 20, "You May Connect to IP");
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(64, 30, WiFi.localIP().toString());
+    display.drawString(64, 46, "Port: " + String(WEBSERVER_PORT));
     display.display();
   }
 
-  unsigned long currentMillis = millis();
+
   if (currentMillis - previousMillisIP > IPInterval) {
     displayIP = false;
+    display.clear();
+    display.display();
   }
 
+  if (currentMillis - previousMillisFeedingTime > FeedingTimeInterval) {
+    for (int i; i < lengthOfFeedingArray; i++) {
+      int temp = feedingHours[i];
+
+      if (!AMPM[i]) {
+        temp += 12;
+      }
+
+      if (temp == getTimeZone(0)) {
+        if (feedingMinutes[i] == timeClient.getMinutes()) {
+          if ((getTimeZone(1) == 0) && sunday[i]) {
+            myservo.attach(D4);
+            myservo.write(180);
+            previousMillisServo = currentMillis;
+            servoInterval = millisPerCup * cups[i];
+            augerOn = true;
+          } else if ((getTimeZone(1) == 1) && monday[i]) {
+            myservo.attach(D4);
+            myservo.write(180);
+            previousMillisServo = currentMillis;
+            servoInterval = millisPerCup * cups[i];
+            augerOn = true;
+          } else if ((getTimeZone(1) == 2) && tuesday[i]) {
+            myservo.attach(D4);
+            myservo.write(180);
+            previousMillisServo = currentMillis;
+            servoInterval = millisPerCup * cups[i];
+            augerOn = true;
+          } else if ((getTimeZone(1) == 3) && wednesday[i]) {
+            myservo.attach(D4);
+            myservo.write(180);
+            previousMillisServo = currentMillis;
+            servoInterval = millisPerCup * cups[i];
+            augerOn = true;
+          } else if ((getTimeZone(1) == 4) && thursday[i]) {
+            myservo.attach(D4);
+            myservo.write(180);
+            previousMillisServo = currentMillis;
+            servoInterval = millisPerCup * cups[i];
+            augerOn = true;
+          } else if ((getTimeZone(1) == 5) && friday[i]) {
+            myservo.attach(D4);
+            myservo.write(180);
+            previousMillisServo = currentMillis;
+            servoInterval = millisPerCup * cups[i];
+            augerOn = true;
+          } else if ((getTimeZone(1) == 6) && saturday[i]) {
+            myservo.attach(D4);
+            myservo.write(180);
+            previousMillisServo = currentMillis;
+            servoInterval = millisPerCup * cups[i];
+            augerOn = true;
+          }
+        }
+      }
+    }
+  }
+
+  if ((currentMillis - previousMillisServo > servoInterval) && augerOn) {
+    myservo.detach();
+    augerOn = false;
+  }
 }
 
 void handleSystemReset() {
@@ -205,7 +343,8 @@ void writeSettings() {
     f.println("www_password=" + String(www_password));
     f.println("otapassword=" + String(OTA_Password));
     f.println("timezone=" + String(timeZone));
-    f.println("24hour=" + String(hour24));
+    //add support for afarray
+    //add support for length of afarray
   }
   f.close();
   readSettings();
@@ -222,10 +361,97 @@ void handleUpdateConfigure() {
   temp.toCharArray(www_password, sizeof(temp));
   OTA_Password = server.arg("otapassword");
   timeZone = server.arg("timezone").toInt();
-  hour24 = server.hasArg("24hour");
 
   writeSettings();
   handleConfigureNoPassword();
+}
+
+void handleUpdateControl() {  //fix this for the control page
+  if (!server.authenticate(www_username, www_password)) {
+    return server.requestAuthentication();
+  }
+
+  int placeholder = server.arg("timeselection").toInt();
+
+  if (server.arg("hours").toInt() == 0) {
+    lengthOfFeedingArray--;
+    if (placeholder == 0) {
+      feedingHours = feedingHours.slice(placeholder + 1, lengthOfFeedingArray);
+      feedingMinutes = feedingMinutes.slice(placeholder + 1, lengthOfFeedingArray);
+      AMPM = AMPM.slice(placeholder + 1, lengthOfFeedingArray);
+      monday = monday.slice(placeholder + 1, lengthOfFeedingArray);
+      tuesday = tuesday.slice(placeholder + 1, lengthOfFeedingArray);
+      wednesday = wednesday.slice(placeholder + 1, lengthOfFeedingArray);
+      thursday = thursday.slice(placeholder + 1, lengthOfFeedingArray);
+      friday = friday.slice(placeholder + 1, lengthOfFeedingArray);
+      saturday = saturday.slice(placeholder + 1, lengthOfFeedingArray);
+      sunday = sunday.slice(placeholder + 1, lengthOfFeedingArray);
+      cups = cups.slice(placeholder + 1, lengthOfFeedingArray);
+    } else if (placeholder == lengthOfFeedingArray) {
+      feedingHours = feedingHours.slice(0, placeholder - 1);
+      feedingMinutes = feedingMinutes.slice(0, placeholder - 1);
+      AMPM = AMPM.slice(0, placeholder - 1);
+      monday = monday.slice(0, placeholder - 1);
+      tuesday = tuesday.slice(0, placeholder - 1);
+      wednesday = wednesday.slice(0, placeholder - 1);
+      thursday = thursday.slice(0, placeholder - 1);
+      friday = friday.slice(0, placeholder - 1);
+      saturday = saturday.slice(0, placeholder - 1);
+      sunday = sunday.slice(0, placeholder - 1);
+      cups = cups.slice(0, placeholder - 1);
+    } else {
+      feedingHours = feedingHours.slice(0, placeholder - 1) + feedingHours.slice(placeholder + 1, lengthOfFeedingArray);
+      feedingMinutes = feedingMinutes.slice(0, placeholder - 1) + feedingMinutes.slice(placeholder + 1, lengthOfFeedingArray);
+      AMPM = AMPM.slice(0, placeholder - 1) + AMPM.slice(placeholder + 1, lengthOfFeedingArray);
+      monday = monday.slice(0, placeholder - 1) + monday.slice(placeholder + 1, lengthOfFeedingArray);
+      tuesday = tuesday.slice(0, placeholder - 1) + tuesday.slice(placeholder + 1, lengthOfFeedingArray);
+      wednesday = wednesday.slice(0, placeholder - 1) + wednesday.slice(placeholder + 1, lengthOfFeedingArray);
+      thursday = thursday.slice(0, placeholder - 1) + thursday.slice(placeholder + 1, lengthOfFeedingArray);
+      friday = friday.slice(0, placeholder - 1) + friday.slice(placeholder + 1, lengthOfFeedingArray);
+      saturday = saturday.slice(0, placeholder - 1) + saturday.slice(placeholder + 1, lengthOfFeedingArray);
+      sunday = sunday.slice(0, placeholder - 1) + sunday.slice(placeholder + 1, lengthOfFeedingArray);
+      cups = cups.slice(0, placeholder - 1) + cups.slice(placeholder + 1, lengthOfFeedingArray);
+    }
+  }
+
+  if ((placeholder == lengthOfFeedingArray) || lengthOfFeedingArray <= 0) {
+    lengthOfFeedingArray++;
+
+    feedingHours.add(server.arg("hours").toInt());
+    feedingMinutes.add(server.arg("minutes").toInt());
+    AMPM.add(server.hasArg("ampm"));
+    monday.add(server.hasArg("monday"));
+    tuesday.add(server.hasArg("tuesday"));
+    wednesday.add(server.hasArg("wednesday"));
+    thursday.add(server.hasArg("thursday"));
+    friday.add(server.hasArg("friday"));
+    saturday.add(server.hasArg("saturday"));
+    sunday.add(server.hasArg("sunday"));
+    double temp = server.arg("cupsoffood").toDouble();
+    cups.add(temp);
+  } else {
+    feedingHours[placeholder] = server.arg("hours").toInt();
+    feedingMinutes[placeholder] = server.arg("minutes").toInt();
+
+    if ((server.arg("ampm").toInt()) == 1) {
+      AMPM[placeholder] = true;
+    } else {
+      AMPM[placeholder] = false;
+    }
+
+    monday[placeholder] = server.hasArg("monday");
+    tuesday[placeholder] = server.hasArg("tuesday");
+    wednesday[placeholder] = server.hasArg("wednesday");
+    thursday[placeholder] = server.hasArg("thursday");
+    friday[placeholder] = server.hasArg("friday");
+    saturday[placeholder] = server.hasArg("saturday");
+    sunday[placeholder] = server.hasArg("sunday");
+    String temp = server.arg("cupsoffood");
+    cups[placeholder] = temp.toDouble();
+  }
+
+  writeSettings();
+  handleControlNoPassword();
 }
 
 void readSettings() {
@@ -255,20 +481,18 @@ void readSettings() {
       timeZone = line.substring(line.lastIndexOf("timezone=") + 9).toInt();
       Serial.println("timezone=" + String(timeZone));
     }
-    if (line.indexOf("24hour=") >= 0) {
-      hour24 = line.substring(line.lastIndexOf("24hour=") + 7).toInt();
-      Serial.println("IS_METRIC=" + String(hour24));
-    }
     if (line.indexOf("otapassword=") >= 0) {
-      hour24 = line.substring(line.lastIndexOf("otapassword=") + 12).toInt();
+      OTA_Password = line.substring(line.lastIndexOf("otapassword=") + 12).toInt();
       Serial.println("otapassword=" + String(OTA_Password));
     }
+    //add suport for afarray
+    //add variable for length of afarray
   }
   fr.close();
 }
 
 void handleNotFound() {
-  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URL in the request
+  server.send(404, "text/plain", "404: <span style='color: red'><strong>N</strong></span>   ot found"); // Send HTTP status 404 (Not Found) when there's no handler for the URL in the request
 }
 
 void handleRoot() {
@@ -286,12 +510,6 @@ void handleConfigure() {
   form.replace("%OTAPASSWORD%", OTA_Password);
   form.replace("%TIMEZONE%", String(timeZone));
 
-  String checked = "";
-  if (hour24) {
-    checked = "checked='checked'";
-  }
-  form.replace("%24HOUR%", checked);
-
   server.send(200, "text/html", form);
 }
 
@@ -302,11 +520,153 @@ void handleConfigureNoPassword() {
   form.replace("%OTAPASSWORD%", OTA_Password);
   form.replace("%TIMEZONE%", String(timeZone));
 
-  String checked = "";
-  if (hour24) {
-    checked = "checked='checked'";
+  server.send(200, "text/html", form);
+}
+
+void handleControl() {
+  if (!server.authenticate(www_username, www_password)) {
+    return server.requestAuthentication();
   }
-  form.replace("%24HOUR%", checked);
+
+  String form = parseControlPage();
+  form.replace("%FEEDINGTIMEMENU%", parseFeedingTimes(lengthOfFeedingArray));
+  form.replace("%FEEDINGTIMES%", reportFeedingTimes());
 
   server.send(200, "text/html", form);
 }
+
+void handleControlNoPassword() {
+  String form = parseControlPage();
+  form.replace("%FEEDINGTIMEMENU%", parseFeedingTimes(lengthOfFeedingArray));
+  form.replace("%FEEDINGTIMES%", reportFeedingTimes());
+
+  server.send(200, "text/html", form);
+}
+
+String reportFeedingTimes() {
+  String temp = "<div class='w3-container'><h2>Feeding Times:";
+  temp.concat(getTimeZone(0));
+  temp.concat(":");
+  temp.concat(timeClient.getMinutes());
+  temp.concat(" ");
+  temp.concat(getTimeZone(1));
+  temp.concat("</h2>");
+
+  for (int i = 0; i < lengthOfFeedingArray; i++) {
+    String temp2 = "<p class='w3-container'>";
+    temp2.concat("<div class='w3-container'><label>Feeding Time: ");
+    temp2.concat((i + 1));
+    temp2.concat("</label></div>");
+
+    temp2.concat("<div class='w3-container'>Time: ");
+    temp2.concat(feedingHours[i]);
+    temp2.concat(":");
+    if (feedingMinutes[i] < 10) {
+      temp2.concat("0");
+    }
+    temp2.concat(feedingMinutes[i]);
+    temp2.concat(" ");
+
+    if (AMPM[i]) {
+      temp2.concat("AM");
+    } else {
+      temp2.concat("PM");
+    }
+
+    temp2.concat("</div>");
+
+    temp2.concat("<div class='w3-container'>");
+    if (monday[i]) {
+      temp2.concat("Mon: <span style='color: green'><strong>Y</strong></span>   ");
+    } else {
+      temp2.concat("Mon: <span style='color: red'><strong>N</strong></span>   ");
+    }
+
+    if (tuesday[i]) {
+      temp2.concat("Tue: <span style='color: green'><strong>Y</strong></span>   ");
+    } else {
+      temp2.concat("Tue: <span style='color: red'><strong>N</strong></span>   ");
+    }
+
+    if (wednesday[i]) {
+      temp2.concat("Wed: <span style='color: green'><strong>Y</strong></span>   ");
+    } else {
+      temp2.concat("Wed: <span style='color: red'><strong>N</strong></span>   ");
+    }
+
+    if (thursday[i]) {
+      temp2.concat("Thur: <span style='color: green'><strong>Y</strong></span>   ");
+    } else {
+      temp2.concat("Thur: <span style='color: red'><strong>N</strong></span>   ");
+    }
+
+    if (friday[i]) {
+      temp2.concat("Fri: <span style='color: green'><strong>Y</strong></span>   ");
+    } else {
+      temp2.concat("Fri: <span style='color: red'><strong>N</strong></span>   ");
+    }
+
+    if (saturday[i]) {
+      temp2.concat("Sat: <span style='color: green'><strong>Y</strong></span>   ");
+    } else {
+      temp2.concat("Sat: <span style='color: red'><strong>N</strong></span>   ");
+    }
+
+    if (sunday[i]) {
+      temp2.concat("Sun: <span style='color: green'><strong>Y</strong></span>   ");
+    } else {
+      temp2.concat("Sun: <span style='color: red'><strong>N</strong></span>   ");
+    }
+
+    temp2.concat("</div>");
+    temp2.concat("<div class='w3-container'>");
+    temp2.concat("Cups: ");
+    temp2.concat(cups[i]);
+    temp2.concat("</div>");
+    temp2.concat("</p>");
+
+    temp.concat(temp2);
+  }
+
+  temp.concat("</div>");
+  return temp;
+}
+
+int getTimeZone(int modes) {
+  int tempHours = timeClient.getHours();
+
+  tempHours += timeZone;
+
+  if (tempHours < 0) {
+    tempHours += 24;
+  } else if (tempHours > 24) {
+    tempHours -= 24;
+  }
+
+  if (modes == 0) {
+    return tempHours;
+  }
+
+  if (modes == 1) {
+    int tempDays = timeClient.getDay();
+    if ((timeClient.getHours() + timeZone) > 24) {
+      tempDays++;
+      return tempDays;
+
+      if (tempDays > 6) {
+        return 0;
+      }
+    } else if ((timeClient.getHours() + timeZone) < 0) {
+      tempDays--;
+      return tempDays;
+
+      if (tempDays < 0) {
+        return 6;
+      }
+    }
+    } else {
+      return timeClient.getDay();
+    }
+  }
+}
+
--- Source/Food-Dispenser/HTML.h
+++ Source/Food-Dispenser/HTML.h
@@ -1,5 +1,7 @@
 String parseHomePage();
 String parseConfigurePage();
+String parseControlPage();
+String parseFeedingTimes(int NumberOfFeedings);
 
 String footer = "<!-- Footer -->"
                 "<footer class='w3-bottom w3-center w3-black w3-padding-small w3-opacity w3-hover-opacity-off'>"
@@ -14,7 +16,7 @@ String footer = "<!-- Footer -->"
 
 String header = "<!DOCTYPE html>"
                 "<html>"
-                "<title>IOT Cloud</title>"
+                "<title>Food Dispenser</title>"
                 "<meta charset='UTF-8'>"
                 "<meta name='viewport' content='width=device-width, initial-scale=1'>"
                 "<link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>"
@@ -41,12 +43,41 @@ String header = "<!DOCTYPE html>"
                 "}"
                 ".w3-wide {letter-spacing: 10px;}"
                 ".w3-hover-opacity {cursor: pointer;}"
+                ".sidenav {"
+                "height: 100%;"
+                "width: 250px;"
+                "position: fixed;"
+                "z-index: 1;"
+                "top: 0;"
+                "left: 0;"
+                "background-color: #111;"
+                "overflow-x: hidden;"
+                "padding-top: 20px;"
+                "}"
+                ".sidenav a {"
+                "padding: 6px 8px 6px 16px;"
+                "text-decoration: none;"
+                "font-size: 25px;"
+                "color: #818181;"
+                "display: block;"
+                "}"
+                ".main {"
+                "margin-left: 250px;"
+                "padding: 0px 10px;"
+                "}"
+                ".checkboxes {"
+                "vertical-align: bottom;"
+                "position: relative;"
+                "top: -17px;"
+                "*overflow: hidden;"
+                "}"
                 "</style>"
                 "<body>"
                 "<!-- Navbar (sit on top) -->"
                 "<div class='w3-top'>"
                 "<div class='w3-bar' id='myNavbar'>"
                 "<a href='Home' class='w3-bar-item w3-button'>HOME</a>"
+                "<a href='Control' class='w3-bar-item w3-button w3-hide-small'><i class='fa fa-user'></i> CONTROL</a>"
                 "<a href='Configure' class='w3-bar-item w3-button w3-hide-small'><i class='fa fa-cogs'></i> CONFIGURE</a>"
                 "<a href='https://github.com/ZGoode/Tie-Fighter-Clock' class='w3-bar-item w3-button w3-hide-small'><i class='fa fa-th'></i> ABOUT</a>"
                 "<a href='/WifiReset' class='w3-bar-item w3-button w3-hide-small w3-right w3-hover-red'>WIFI RESET</a>"
@@ -57,7 +88,7 @@ String header = "<!DOCTYPE html>"
 String homePage = "<!-- First Parallax Image with Logo Text -->"
                   "<div class='bgimg w3-display-container w3-opacity-min' id='home'>"
                   "<div class='w3-display-middle' style='white-space:nowrap;'>"
-                  "<p><span class='w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity'>TIE<span class='w3-hide-small'> FIGHTER</span> CLOCK</span></p>"
+                  "<p><span class='w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity'>Food<span class='w3-hide-small'> Dispenser</span></p>"
                   "</div>"
                   "</div>";
 
@@ -76,10 +107,62 @@ String configurePage = "<div class='bgimg w3-display-container w3-opacity-min' i
                        "</div>"
                        "</div>";
 
+String controlPage = "<div class='bgimg w3-display-container w3-opacity-min main' id='home'>"
+                     "<div class='sidenav'>"
+                     "<form class='w3-container' action='/updateControl' method='get'>"
+                     "<h2>Feeding Config:</h2>"
+                     "<label for='timeselection'>Select Feeding Time: </label><select id='timeselection' name='timeselection'>"
+                     "%FEEDINGTIMEMENU%"
+                     "</select>"
+                     "<p><label>Hours : Minutes </label><input class='w3-input w3-border w3-margin-bottom' type='text' name='hours' value='0'><input class='w3-input w3-border w3-margin-bottom' type='text' name='minutes' value='0'>"
+                     "<select id='ampm' name='ampm'><option value='1'>AM</option><option value='2'>PM</option></select></p>"
+                     "<label>Monday </label><input class='w3-input w3-border w3-margin-bottom checkboxes' type='checkbox' name='monday'>"
+                     "<label>Tuesday </label><input class='w3-input w3-border w3-margin-bottom checkboxes' type='checkbox' name='tuesday'>"
+                     "<label>Wednesday </label><input class='w3-input w3-border w3-margin-bottom checkboxes' type='checkbox' name='wednesday'>"
+                     "<label>Thursday </label><input class='w3-input w3-border w3-margin-bottom checkboxes' type='checkbox' name='thursday'>"
+                     "<label>Friday </label><input class='w3-input w3-border w3-margin-bottom checkboxes' type='checkbox' name='friday'>"
+                     "<label>Saturday </label><input class='w3-input w3-border w3-margin-bottom checkboxes' type='checkbox' name='saturday'>"
+                     "<label>Sunday </label><input class='w3-input w3-border w3-margin-bottom checkboxes' type='checkbox' name='sunday'>"
+                     "<label>Cups of Food: </label><input class='w3-input w3-border w3-margin-bottom' type='text' name='cupsoffood' value='0'></p>"
+                     "<button class='w3-button w3-block w3-grey w3-section w3-padding' type='submit'>Save</button>"
+                     "</form>"
+                     "</div>"
+                     "<div class='w3-display-middle' style='white-space:nowrap;'>"
+                     "<div class='w3-black'>"
+                     "%FEEDINGTIMES%"
+                     "</div>"
+                     "</div>"
+                     "</div>";
+
 String parseHomePage() {
   return header + homePage + footer;
 }
 
 String parseConfigurePage() {
   return header + configurePage + footer;
+}
+
+String parseControlPage() {
+  return header + controlPage + footer;
+}
+
+String parseFeedingTimes(int NumberOfFeedings) {
+  String temp = "";
+
+  for (int i = 0; i < NumberOfFeedings; i++) {
+    String temp2 = "<option value='";
+
+    temp2.concat(i);
+    temp2.concat("'>");
+    temp2.concat((i + 1));
+    temp2.concat("</option>");
+
+    temp.concat(temp2);
+  }
+
+  temp.concat("<option value='");
+  temp.concat(NumberOfFeedings);
+  temp.concat("'>New</option>");
+
+  return temp;
 }
